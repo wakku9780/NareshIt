1541b0b3ac2dbd97681e95c3afd325cf365cd146
waqar <wakku9780@gmail.com>
2024-02-21 16:07:32 +0530

    Add project files.

---
+++ NareshIt/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NareshIt
+{
+    internal class Customer
+    {
+       public int CustId;
+        bool Status;
+        string Cname;
+        double Balance;
+        public Customer(int CustId, bool Status,string Cname,double Balance)
+        {
+            this.CustId = CustId;
+            this.Status = Status;
+            this.Cname  =Cname;
+            this.Balance = Balance;
+        }
+
+
+    }
+}
---
+++ NareshIt/TestCustomers.cs
@@ -0,0 +1,14 @@
+﻿namespace NareshIt
+{
+    internal class TestCustomers
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer(101, false, "JOHN", 5000.000);
+            Console.WriteLine("CustomerId: " + obj.CustId);
+            obj.CustId = 102;
+            Console.WriteLine("After Change: " + obj.CustId);
+            Console.ReadLine();
+        }
+    }
+}
